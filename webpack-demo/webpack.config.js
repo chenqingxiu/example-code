287fe000e5ad43be07d5d914dc9809337abcd710
chenqingxiu <chenqingxiu0592@gmail.com>
2017-11-30 23:09:19 +0800

    webpack-demo1130

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,7 +1,45 @@
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+
 module.exports = {
-  entry: './src/script/main.js',
+  entry: {
+    main: './src/script/main.js',
+    a: './src/script/a.js',
+    b: './src/script/b.js',
+    c: './src/script/c.js'
+  },
+
   output: {
     path: __dirname+'/dist',
-    filename: 'bundle.js'
-  }
+    filename: 'js/[name]-[chunkhash].js'
+  },
+
+  plugins: [
+    new htmlWebpackPlugin({
+      filename: 'a.html',
+      template: 'index.html',
+      inject: 'body',
+      title: 'a is good',
+      date: new Date(),
+      chunks: ['main', 'a']
+    }),
+
+    new htmlWebpackPlugin({
+      filename: 'b.html',
+      template: 'index.html',
+      inject: 'body',
+      title: 'b is good',
+      date: new Date(),
+      chunks: ['main', 'b']
+    }),
+
+    new htmlWebpackPlugin({
+      filename: 'c.html',
+      template: 'index.html',
+      inject: 'body',
+      title: 'c is good',
+      date: new Date(),
+      chunks: ['main', 'c']
+    })
+  ]
 }
